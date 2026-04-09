d702f6f3fa075f44ddb3840529dca51f544f5886
Artur Drzazga <artur.drzazga03@gmail.com>
2026-04-09 17:57:06 +0200

    Fix for user registration error

--- users/serializers.py
+++ users/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["id", "email", "username", "first_name", "last_name", "is_staff"]
+        fields = ["id", "email", "first_name", "last_name"]
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
