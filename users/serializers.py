d4a34ab252f3946952468f7ed94369556b266064
Artur Drzazga <artur.drzazga03@gmail.com>
2026-04-14 15:21:06 +0200

    Add ruff formatting and configuration

--- users/serializers.py
+++ users/serializers.py
@@ -4,8 +4,8 @@ from rest_framework import serializers
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
 
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "email", "first_name", "last_name", "password"]
