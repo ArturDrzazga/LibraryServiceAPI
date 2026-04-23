3215d93732b6827b9c21ea3523b914016bf5ffda
Artur Drzazga <artur.drzazga03@gmail.com>
2026-04-23 17:05:34 +0200

    Add sample objects for tests

--- borrowings/tests.py
+++ borrowings/tests.py
@@ -1,3 +1,29 @@
+from datetime import date, timedelta
+
 from django.test import TestCase
 
-# Create your tests here.
+from books.models import Book
+from borrowings.models import Borrowing
+
+BORROWINGS_URL = "/api/borrowings/"
+
+def sample_book(**params):
+    defaults = {
+        "title": "Test Book",
+        "author": "Test Author",
+        "cover": "SOFT",
+        "inventory": 5,
+        "daily_fee": 1.50,
+    }
+    defaults.update(params)
+    return Book.objects.create(**defaults)
+
+def sample_borrowing(user, book, **params):
+    defaults = {
+        "borrow_date": date.today(),
+        "expected_return_date": date.today() + timedelta(days=7),
+        "book": book,
+        "user": user,
+    }
+    defaults.update(params)
+    return Borrowing.objects.create(**defaults)
